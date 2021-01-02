fb8091b5445ca58da18f1110ffb5b6a4faa584a1
Manal El-rhezzali <manal.gzl99@gmail.com>
2021-01-02 21:25:04 +0100

    commiting the whole project at once

---
+++ Gestion_Service_ENSA/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Gestion_Service_ENSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.conx = new System.Windows.Forms.Button();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Floorpanel = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.Password = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.Floorpanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.conx);
+            this.panel1.Controls.Add(this.pass);
+            this.panel1.Controls.Add(this.login);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.Floorpanel);
+            this.panel1.Controls.Add(this.Password);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-5, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(512, 613);
+            this.panel1.TabIndex = 11;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 71);
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // conx
+            // 
+            this.conx.BackColor = System.Drawing.Color.Aqua;
+            this.conx.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.conx.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.conx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conx.Image = ((System.Drawing.Image)(resources.GetObject("conx.Image")));
+            this.conx.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.conx.Location = new System.Drawing.Point(144, 482);
+            this.conx.Name = "conx";
+            this.conx.Size = new System.Drawing.Size(183, 60);
+            this.conx.TabIndex = 18;
+            this.conx.Text = "Connexion";
+            this.conx.UseVisualStyleBackColor = false;
+            this.conx.Click += new System.EventHandler(this.conx_Click_1);
+            // 
+            // pass
+            // 
+            this.pass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.pass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass.ForeColor = System.Drawing.SystemColors.Window;
+            this.pass.Location = new System.Drawing.Point(96, 342);
+            this.pass.Multiline = true;
+            this.pass.Name = "pass";
+            this.pass.PasswordChar = '●';
+            this.pass.Size = new System.Drawing.Size(301, 32);
+            this.pass.TabIndex = 17;
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.login.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.login.Font = new System.Drawing.Font("Bahnschrift SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login.ForeColor = System.Drawing.SystemColors.Window;
+            this.login.Location = new System.Drawing.Point(95, 242);
+            this.login.Multiline = true;
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(301, 32);
+            this.login.TabIndex = 16;
+            this.login.TextChanged += new System.EventHandler(this.login_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Aqua;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Location = new System.Drawing.Point(96, 320);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 4);
+            this.panel2.TabIndex = 15;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Aqua;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel3.Location = new System.Drawing.Point(-2, -2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(300, 4);
+            this.panel3.TabIndex = 14;
+            // 
+            // Floorpanel
+            // 
+            this.Floorpanel.BackColor = System.Drawing.Color.Aqua;
+            this.Floorpanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Floorpanel.Controls.Add(this.panel5);
+            this.Floorpanel.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Floorpanel.Location = new System.Drawing.Point(96, 223);
+            this.Floorpanel.Name = "Floorpanel";
+            this.Floorpanel.Size = new System.Drawing.Size(300, 4);
+            this.Floorpanel.TabIndex = 14;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Aqua;
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel5.Location = new System.Drawing.Point(-2, -2);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(300, 4);
+            this.panel5.TabIndex = 14;
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.ForeColor = System.Drawing.SystemColors.Control;
+            this.Password.Location = new System.Drawing.Point(91, 296);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(82, 21);
+            this.Password.TabIndex = 1;
+            this.Password.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(94, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(452, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 55);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 611);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Gestion de Services ENSA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.Floorpanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel Floorpanel;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button conx;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
